fea6a3d1e974fe6e2ab8d0981a18d52ccf1d26d5
Egunbang <dlejsdnd@gmail.com>
2024-09-25 19:10:05 +0900

    si단위 찐막
    si단위 찐막

--- si단위계변환/si단위계변환/소스.cpp
+++ si단위계변환/si단위계변환/소스.cpp
@@ -2,14 +2,20 @@
 
 int main(void)
 {
-	float meter, kilogram;
+	float meter,inch, kilogram;
 	printf("미터 단위의 길이를 입력하시오 : ");
 	scanf_s(" %f", &meter);
 	printf("%.2f미터는 %.2f피트입니다\n", meter, meter * 3.28084);
 
+	printf("미터 단위의 길이를 입력하시오 : ");
+	scanf_s(" %f", &inch);
+	printf("%.2f미터는 %.2f인치입니다\n", inch, meter * 0.0254);
+	
+	
+	
 	printf("킬로그램 단위의 무게를 입력하시오");
 	scanf_s("%f", &kilogram);
-	printf("%.2f킬로그램은 %.2f파운드이다\n", kilogram, kilogram * 2.20462);
+	printf("%.2f킬로그램은 %.2fslug이다\n", kilogram, kilogram * 0.0685);
 	return 0;
 
 }
